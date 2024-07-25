8c0de174f8cb7cfa28c5538db686e034cb3372d7
mohamed ahmed 3yad <60317041+m7mda3yad@users.noreply.github.com>
2024-07-25 13:43:28 +0300

    First Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             // Other seeders...
-            // PharmacySeeder::class,
-            // ProductSeeder::class,
+             PharmacySeeder::class,
+             ProductSeeder::class,
             PharmacyProductSeeder::class,
         ]);
 
